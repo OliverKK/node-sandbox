95d8847d6c845baaa19aa92cec5e9da3ca91099d
OliverKK <oliver.kinderknecht@googlemail.com>
2020-07-19 09:36:42 +0200

    Change url

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const replace = require('replace-in-file');
 const options = {
   files: 'statics/index.html',
   from: [/MARKET/g, /CATALOGID/g],
-  to: ['de', '1afcaa70-c578-4c2e-b7e4-2d341b04ed4f'],
+  to: ['de', '1afcaa70-c578-4c2e-b7e4-2d341b04ed4f'], 
 };
 
 replace(options)
