1e1dbe96a547771e43021b1ef764d925200fb7be
OliverKK <oliver.kinderknecht@googlemail.com>
2020-07-19 09:03:15 +0200

    {INITIAL_COMMIT}

---
+++ app.js
@@ -0,0 +1,15 @@
+//Load the library and specify options
+const replace = require('replace-in-file');
+const options = {
+  files: 'statics/index.html',
+  from: [/MARKET/g, /CATALOGID/g],
+  to: ['de', '1afcaa70-c578-4c2e-b7e4-2d341b04ed4f'],
+};
+
+replace(options)
+  .then(results => {
+    console.log('Replacement results:', results);
+  })
+  .catch(error => {
+    console.error('Error occurred:', error);
+  });
